1caf61546642890379b3a8b0136485ee56cf5be3
SaiSanthoshS <sssanthosh.cse@gmail.com>
2023-06-24 04:57:13 +0530

    up

--- preprocessing/jupyter_utils.py
+++ preprocessing/jupyter_utils.py
@@ -1,19 +1,22 @@
 import os
 from file_utils import preprocess_code_file
-
-import nbformat
-from nbconvert import PythonExporter
+from jupyter_client.kernelspec import KernelSpecManager
+from jupyter_client.kernelspec import NoSuchKernel
 
 def convert_notebook_to_script(notebook_path, script_path):
     """Convert a Jupyter notebook to a Python script."""
-    with open(notebook_path, 'r') as notebook_file:
-        notebook = nbformat.read(notebook_file, as_version=nbformat.NO_CONVERT)
+    kernel_name = get_notebook_kernel(notebook_path)
+    cmd = f"jupyter nbconvert --to python --execute --ExecutePreprocessor.kernel_name={kernel_name} --output {script_path} {notebook_path}"
+    os.system(cmd)
 
-    exporter = PythonExporter()
-    code, _ = exporter.from_notebook_node(notebook)
-
-    with open(script_path, 'w') as script_file:
-        script_file.write(code)
+def get_notebook_kernel(notebook_path):
+    """Get the kernel name of a Jupyter notebook."""
+    kernel_spec_manager = KernelSpecManager()
+    try:
+        kernel_spec = kernel_spec_manager.get_kernel_spec_for_nbconvert(notebook_path)
+        return kernel_spec.language
+    except NoSuchKernel:
+        raise ValueError("Unable to determine the kernel for the notebook.")
 
 def preprocess_jupyter_notebook(notebook_path, max_tokens):
     """Preprocess a Jupyter notebook by converting it to a script and truncating it if it exceeds the token limit."""
@@ -21,3 +24,4 @@ def preprocess_jupyter_notebook(notebook_path, max_tokens):
     convert_notebook_to_script(notebook_path, script_path)
     preprocess_code_file(script_path, max_tokens)
     os.remove(notebook_path)
+
