eb17740983c1d40ef4221e6a47a53dde7d7b3805
SaiSanthoshS <sssanthosh.cse@gmail.com>
2023-06-24 03:58:00 +0530

    basic file utils description

---
+++ preprocessing/file_utils.py
@@ -0,0 +1,30 @@
+import os
+
+def get_file_size(file_path):
+    """Get the size of a file in bytes."""
+    return os.path.getsize(file_path)
+
+def delete_file(file_path):
+    """Delete a file from the file system."""
+    os.remove(file_path)
+
+def get_file_extension(file_path):
+    """Get the extension of a file."""
+    return os.path.splitext(file_path)[1]
+
+def is_code_file(file_path):
+    """Check if a file is a code file based on its extension."""
+    valid_extensions = ['.py', '.ipynb']
+    file_extension = get_file_extension(file_path)
+    return file_extension.lower() in valid_extensions
+
+def preprocess_code_file(file_path, max_tokens):
+    """Preprocess a code file by truncating it if it exceeds the token limit."""
+    with open(file_path, 'r') as file:
+        code = file.read()
+
+    code_lines = code.split('\n')
+    if len(code_lines) > max_tokens:
+        truncated_code = '\n'.join(code_lines[:max_tokens])
+        with open(file_path, 'w') as file:
+            file.write(truncated_code)
