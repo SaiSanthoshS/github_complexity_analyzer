4bed0e41dea009c5086cffbc7dd48d5b26f811b1
SaiSanthoshS <sssanthosh.cse@gmail.com>
2023-06-24 03:59:52 +0530

    conversion of notebook to python

---
+++ preprocessing/jupyter_utils.py
@@ -0,0 +1,20 @@
+import nbformat
+from nbconvert import PythonExporter
+
+def convert_notebook_to_script(notebook_path, script_path):
+    """Convert a Jupyter notebook to a Python script."""
+    with open(notebook_path, 'r') as notebook_file:
+        notebook = nbformat.read(notebook_file, as_version=nbformat.NO_CONVERT)
+
+    exporter = PythonExporter()
+    code, _ = exporter.from_notebook_node(notebook)
+
+    with open(script_path, 'w') as script_file:
+        script_file.write(code)
+
+def preprocess_jupyter_notebook(notebook_path, max_tokens):
+    """Preprocess a Jupyter notebook by converting it to a script and truncating it if it exceeds the token limit."""
+    script_path = notebook_path.replace('.ipynb', '.py')
+    convert_notebook_to_script(notebook_path, script_path)
+    preprocess_code_file(script_path, max_tokens)
+    os.remove(notebook_path)
